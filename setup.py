e7a52511c1725ba39d1ceaa47b9d0be119d17484
Andrew Lee <andrew-lee-zuora@users.noreply.github.com>
2020-03-26 20:52:23 -0700

    WIP add support for using db instead of tags for recording ztid

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
         name='zsec-aws-tools',
         packages=['zsec_aws_tools'],
-        install_requires=['boto3', 'toolz'],
+        install_requires=['boto3', 'toolz', 'pynamodb'],
         tests_require=['toolz', 'pytest'],
         version='v0.1.21-alpha.1',
         )
