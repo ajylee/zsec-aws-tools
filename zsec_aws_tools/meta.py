d630d2c7f6ded903a42f4e530a79a732d1c80a16
Andrew Lee <andrew-lee-zuora@users.noreply.github.com>
2020-08-28 12:43:07 -0700

    Reformat with black

--- zsec_aws_tools/meta.py
+++ zsec_aws_tools/meta.py
@@ -1,21 +1,23 @@
+from __future__ import annotations
 import functools
-from typing import Tuple, Generator
+from typing import Tuple, Generator, ClassVar, Type
 
 import boto3
 import botocore.model
 import pynamodb.models
 from pynamodb.attributes import UnicodeAttribute
 
-type_name_mapping = {'string': str,
-                     'long': int,
-                     'blob': bytes,
-                     'list': list,
-                     'double': int,
-                     'structure': dict,
-                     'map': dict,
-                     'integer': int,
-                     'timestamp': str,  # not sure about this
-                     }
+type_name_mapping = {
+    "string": str,
+    "long": int,
+    "blob": bytes,
+    "list": list,
+    "double": int,
+    "structure": dict,
+    "map": dict,
+    "integer": int,
+    "timestamp": str,  # not sure about this
+}
 
 
 def get_operation_model(svc, fn):
@@ -32,19 +34,25 @@ def get_operation_model(svc, fn):
     return svc._service_model.operation_model(svc._PY_TO_OP_NAME[func_name])
 
 
-def apply_with_relevant_kwargs(svc, fn, kwargs, ignore_when_missing_required_keys=False):
+def apply_with_relevant_kwargs(
+    svc, fn, kwargs, ignore_when_missing_required_keys=False
+):
     operation_model = get_operation_model(svc, fn)
     desired_keys = operation_model.input_shape.members.keys()
     filtered_kwargs = {k: v for k, v in kwargs.items() if k in desired_keys}
     required_keys = operation_model.input_shape.required_members
 
-    if ignore_when_missing_required_keys and not frozenset(kwargs.keys()).issuperset(required_keys):
+    if ignore_when_missing_required_keys and not frozenset(kwargs.keys()).issuperset(
+        required_keys
+    ):
         return
     else:
         return fn(**filtered_kwargs)
 
 
-def get_parameter_shapes(service_client, *operation_names: str) -> Generator[Tuple[str, botocore.model.Shape], None, None]:
+def get_parameter_shapes(
+    service_client, *operation_names: str
+) -> Generator[Tuple[str, botocore.model.Shape], None, None]:
     for operation_name in operation_names:
         operation_model = get_operation_model(service_client, operation_name)
         for key, shape in operation_model.input_shape.members.items():
@@ -81,13 +89,13 @@ class PartialModel(pynamodb.models.Model):
 
     @classmethod
     def set_table_name(cls, session: boto3.Session, table_name: str, **kwargs):
-        ssm = session.client('ssm')
+        ssm = session.client("ssm")
         ssm.put_parameter(
             Name=cls.table_parameter_name,
-            Description='Name of the table containing resources created by fleet former, indexed by ZRN.',
+            Description="Name of the table containing resources created by fleet former, indexed by ZRN.",
             Value=table_name,
-            Type='String',
-            AllowedPattern=r'[\w_-]+$',
+            Type="String",
+            AllowedPattern=r"[\w_-]+$",
             **kwargs
         )
 
